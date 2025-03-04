1fedb3753be81b370035dcd89425078a2fbb0672
polyprogrammist <vad.e.volodin@gmail.com>
2025-03-04 15:33:53 +0800

    something with openapi

---
+++ testokplain/src/main.rs
@@ -0,0 +1,40 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use near_time;
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+struct MyResponse {
+    message: String,
+    code: u16,
+}
+
+use near_jsonrpc_primitives::types::transactions::{
+    RpcSendTransactionRequest, RpcTransactionResponse
+};
+
+use okapi::openapi3::{OpenApi, SchemaObject};
+use schemars::gen::SchemaGenerator;
+use schemars::schema::RootSchema;
+
+fn main() {
+    // Generate schema
+    let root_schema = schemars::gen::SchemaGenerator::default().into_root_schema_for::<RpcTransactionResponse>();
+
+    // Create OpenAPI spec
+    let openapi = OpenApi {
+        openapi: "3.0.0".to_string(),
+        info: okapi::openapi3::Info {
+            title: "My API".to_string(),
+            version: "1.0.0".to_string(),
+            ..Default::default()
+        },
+        components: Some(okapi::openapi3::Components {
+            schemas: root_schema.definitions.into_iter().map(|(k, v)| (k, v.into())).collect(),
+            ..Default::default()
+        }),
+        ..Default::default()
+    };
+
+    let spec_yaml = serde_yaml::to_string(&openapi).unwrap();
+    println!("{}", spec_yaml);
+}
