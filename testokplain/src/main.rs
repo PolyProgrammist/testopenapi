174141fce35bd4759495c14eaca3e27175741dd8
polyprogrammist <vad.e.volodin@gmail.com>
2025-03-13 18:02:39 +0800

    tmp

--- testokplain/src/main.rs
+++ testokplain/src/main.rs
@@ -23,15 +23,31 @@ struct JsonRpcResponse<T> {
     id: String,
 } 
 
+
+#[derive(JsonSchema)]
+struct RpcParams {
+    request_data: near_jsonrpc_primitives::types::transactions::RpcTransactionStatusRequest,
+    fetch_receipt: bool,
+}
+
+
+#[derive(JsonSchema)]
+struct JsonRpcRequest<T> {
+    jsonrpc: String,
+    id: String,
+    params: T,
+    const tmp: String = "hello",
+}
+
 fn generate_schema<T: JsonSchema>() -> OpenApi {
     let settings = schemars::gen::SchemaSettings::openapi3();
     let mut generator = schemars::gen::SchemaGenerator::new(settings);
 
-    let root_schema = generator.into_root_schema_for::<JsonRpcResponse<T>>();
+    let root_schema = generator.into_root_schema_for::<T>();
 
     let mut theMap: okapi::Map<String, okapi::openapi3::SchemaObject> = root_schema.definitions.into_iter().map(|(k, v)| (k, v.into())).collect();
-    theMap.insert("JsonRpcResponse".to_string(), root_schema.schema);
-
+    theMap.insert(T::schema_name(), root_schema.schema);
+    println!("{:?}", T::schema_name());
 
     OpenApi {
         openapi: "3.0.0".to_string(),
@@ -49,11 +65,12 @@ fn generate_schema<T: JsonSchema>() -> OpenApi {
 }
 
 fn main() {
-    let openapi = generate_schema::<RpcTransactionResponse>();
+    let response_schema = generate_schema::<JsonRpcResponse<RpcTransactionResponse>>();
+    let request_schema = generate_schema::<JsonRpcRequest<RpcParams>>();
     
     // let spec_json = serde_json::to_string_pretty(&openapi).unwrap();
     // println!("{}", spec_json);
 
-    let spec_yaml = serde_yaml::to_string(&openapi).unwrap();
-    println!("{}", spec_yaml);
+    let spec_yaml = serde_yaml::to_string(&response_schema).unwrap();
+    // println!("{}", spec_yaml);
 }
