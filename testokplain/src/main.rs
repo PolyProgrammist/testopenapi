f73989b91fb5142ee80223079a1f65a538dda5b7
polyprogrammist <vad.e.volodin@gmail.com>
2025-03-18 19:27:23 +0700

    remove warnings

--- testokplain/src/main.rs
+++ testokplain/src/main.rs
@@ -37,13 +37,13 @@ pub enum ResponseEither<T, E> {
     RpcError { error: RpcError }
 }
 
-
 #[derive(JsonSchema)]
+#[allow(dead_code)] // Suppress fields never read
 struct JsonRpcResponse<T, E> {
     jsonrpc: String,
     id: String,
     #[serde(flatten)]
-    reserr: ResponseEither<T, E>,
+    response_or_error: ResponseEither<T, E>,
 }
 
 trait MethodNameTrait {
@@ -51,6 +51,7 @@ trait MethodNameTrait {
 }
 
 #[derive(JsonSchema)]
+#[allow(dead_code)] // Suppress fields never read
 struct JsonRpcRequest<T: MethodNameTrait> {
     jsonrpc: String,
     id: String,
@@ -61,18 +62,18 @@ struct JsonRpcRequest<T: MethodNameTrait> {
 type SchemasMap = okapi::Map::<String, okapi::openapi3::SchemaObject>;
 type PathsMap = okapi::Map::<String, okapi::openapi3::PathItem>;
 
-fn schema_map<T: JsonSchema>() -> SchemasMap {
+fn schemas_map<T: JsonSchema>() -> SchemasMap {
     let settings = schemars::gen::SchemaSettings::openapi3();
-    let mut generator = schemars::gen::SchemaGenerator::new(settings);
+    let generator = schemars::gen::SchemaGenerator::new(settings);
 
     let root_schema = generator.into_root_schema_for::<T>();
 
-    let mut theMap: SchemasMap = root_schema.definitions.into_iter().map(|(k, v)| (k, v.into())).collect();
-    theMap.insert(T::schema_name(), root_schema.schema);
-    theMap
+    let mut result: SchemasMap = root_schema.definitions.into_iter().map(|(k, v)| (k, v.into())).collect();
+    result.insert(T::schema_name(), root_schema.schema);
+    result
 }
 
-fn get_paths(request_schema_name: String, response_schema_name: String, method_name: String) -> PathsMap {
+fn paths_map(request_schema_name: String, response_schema_name: String, method_name: String) -> PathsMap {
     let request_body = okapi::openapi3::RequestBody {
         required: true,
         content: {
@@ -127,24 +128,28 @@ fn get_paths(request_schema_name: String, response_schema_name: String, method_n
     paths
 }
 
-fn path_spec_internal<RequestType: JsonSchema, ResponseType: JsonSchema>(allSchemas: &mut SchemasMap, allPaths: &mut PathsMap, method_name: String) {
-    let mut requestMap = schema_map::<RequestType>();
-    let responseMap = schema_map::<ResponseType>();
+fn add_spec_for_path_internal<RequestType: JsonSchema, ResponseType: JsonSchema>(all_schemas: &mut SchemasMap, all_paths: &mut PathsMap, method_name: String) {
+    let request_map = schemas_map::<RequestType>();
+    let response_map = schemas_map::<ResponseType>();
 
-    let mut allMap = requestMap;
-    allMap.extend(responseMap);
+    let mut schemas = request_map;
+    schemas.extend(response_map);
 
-    let paths = get_paths(
+    let paths = paths_map(
         format!("#/components/schemas/{}", RequestType::schema_name()), 
         format!("#/components/schemas/{}", ResponseType::schema_name()),
         method_name
     );
 
-    allSchemas.extend(allMap.clone());
-    allPaths.extend(paths.clone());
+    all_schemas.extend(schemas.clone());
+    all_paths.extend(paths.clone());
 }
 
-fn whole_spec(allSchemas: SchemasMap, allPaths: PathsMap) -> OpenApi {
+fn add_spec_for_path<Request: JsonSchema + MethodNameTrait, Response: JsonSchema>(all_schemas: &mut SchemasMap, all_paths: &mut PathsMap, method_name: String) {
+    add_spec_for_path_internal::<JsonRpcRequest<Request>, JsonRpcResponse<Response, RpcError>>(all_schemas, all_paths, method_name)
+}
+
+fn whole_spec(all_schemas: SchemasMap, all_paths: PathsMap) -> OpenApi {
     OpenApi {
         openapi: "3.0.0".to_string(),
         info: okapi::openapi3::Info {
@@ -152,17 +157,13 @@ fn whole_spec(allSchemas: SchemasMap, allPaths: PathsMap) -> OpenApi {
             version: "1.0.0".to_string(),
             ..Default::default()
         },
-        paths: allPaths,
+        paths: all_paths,
         components: Some(okapi::openapi3::Components {
-            schemas: allSchemas,
+            schemas: all_schemas,
             ..Default::default()
         }),
         ..Default::default()
     }
-}
-
-fn path_spec<Request: JsonSchema + MethodNameTrait, Response: JsonSchema>(allSchemas: &mut SchemasMap, allPaths: &mut PathsMap, method_name: String) {
-    path_spec_internal::<JsonRpcRequest<Request>, JsonRpcResponse<Response, RpcError>>(allSchemas, allPaths, method_name)
 }
 
 
@@ -180,33 +181,33 @@ use near_jsonrpc_primitives::types::{
 //// ---- implement for all requests ----- 
 
 #[derive(Serialize, Deserialize, JsonSchema)]
-pub enum Tx_enum {
+pub enum TxMethodNameHelperEnum {
     #[serde(rename = "tx")]
     VALUE
 }
 
 impl MethodNameTrait for RpcTransactionStatusRequest {
-    type S = Tx_enum;
+    type S = TxMethodNameHelperEnum;
 }
 
 #[derive(Serialize, Deserialize, JsonSchema)]
-pub enum Block_enum {
+pub enum BlockMethodNameHelperEnum {
     #[serde(rename = "block")]
     VALUE
 }
 
 impl MethodNameTrait for RpcBlockRequest {
-    type S = Block_enum;
+    type S = BlockMethodNameHelperEnum;
 }
 
 fn main() {
-    let mut allSchemas = SchemasMap::new();
-    let mut allPaths = PathsMap::new();
+    let mut all_schemas = SchemasMap::new();
+    let mut all_paths = PathsMap::new();
 
-    path_spec::<RpcTransactionStatusRequest, RpcTransactionResponse>(&mut allSchemas, &mut allPaths, "tx".to_string());
-    path_spec::<RpcBlockRequest, RpcBlockResponse>(&mut allSchemas, &mut allPaths, "block".to_string());
+    add_spec_for_path::<RpcTransactionStatusRequest, RpcTransactionResponse>(&mut all_schemas, &mut all_paths, "tx".to_string());
+    add_spec_for_path::<RpcBlockRequest, RpcBlockResponse>(&mut all_schemas, &mut all_paths, "block".to_string());
 
-    let path_schema = whole_spec(allSchemas, allPaths);
+    let path_schema = whole_spec(all_schemas, all_paths);
     
     let spec_json = serde_json::to_string_pretty(&path_schema).unwrap();
     println!("{}", spec_json);
